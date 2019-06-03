72d70de4161b42365b8ff560d0100247c907f15d
KaterynaKozlianska <kkozlianska@gmail.com>
2019-06-04 00:05:40 +0300

    del test_np

--- test_np.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-df=pd.DataFrame(np.arange(0,32,2).reshape(4,4))
-df.index=['A','b','c','d']
-df.columns=['q','w','e','r']
-df.index.name='index'
-df=df.rename( {'A':'a'}, axis=0)
-print (df)
-print(df.d)
